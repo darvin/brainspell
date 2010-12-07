710657b9958e5eab50be2157a632ff15b10d3010
Sergey Klimov <dcdarv@gmail.com>
2010-12-07 04:25:40 +0200

    added svg, many changes

---
+++ setup.py
@@ -0,0 +1,162 @@
+#!/usr/bin/env python
+"""
+py2app/py2exe build script for MyApplication.
+
+Will automatically ensure that all build prerequisites are available
+via ez_setup
+
+Usage (Mac OS X):
+    python setup.py py2app
+
+Usage (Windows):
+    python setup.py py2exe
+
+Usage (Ubuntu/Debian):
+    python setup.py --command-packages=stdeb.command bdist_deb
+
+Usage (RPM-based linux distros):
+    python setup.py bdist_rpm
+"""
+import glob
+from distutils.core import setup
+from distutils.core import Command
+from distutils.command.build import build
+import sys
+import os
+
+mainscript = 'src/cryotec_client.pyw'
+
+def needsupdate(src, targ):
+    return not os.path.exists(targ) or os.path.getmtime(src) > os.path.getmtime(targ)
+
+
+class CryotecClientBuildUi(Command):
+    description = "build Python modules from qrc files"
+
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def compile_qrc(self, qrc_file, py_file):
+        if not needsupdate(qrc_file, py_file):
+            return
+        print("compiling %s -> %s" % (qrc_file, py_file))
+        try:
+            import subprocess
+            rccprocess = subprocess.Popen(['pyrcc4', qrc_file, '-o', py_file])
+            rccprocess.wait()
+        except Exception, e:
+            raise distutils.errors.DistutilsExecError, 'Unable to compile resouce file %s' % str(e)
+            return
+    def run(self):
+        self.compile_qrc( 'resources/images/images.qrc', 'src/images_rc.py' )
+#        self.compile_qrc( 'resources/translations/translations.qrc', 'src/translation_rc.py' )
+
+
+class CryotecClientBuild(build):
+    def is_win_platform(self):
+        return hasattr(self, "plat_name") and (self.plat_name[:3] == 'win')
+
+    sub_commands = [('build_ui', None)] + build.sub_commands
+    #+ [('build_winext', is_win_platform)]
+
+
+cmds = {
+        'build' : CryotecClientBuild,
+        'build_ui' : CryotecClientBuildUi,
+        }
+
+
+base_options = dict (name=u'cryotec_client',
+      install_requires = ["pyqt",],
+      version="1.0",
+      description='Cryotec Client',
+      author='Sergey Klimov',
+      author_email='dcdarv@gmail.com',
+      url='http://github.com/darvin/cryotec_client',
+      package_dir = {'cryotec_client': 'src'},
+      packages=['cryotec_client'],
+
+      data_files=[('doc',glob.glob("doc/*.html")),
+                  ('doc/images',glob.glob('doc/images/*.png')),
+  ],
+      long_description="""Cryotec Client""",
+      license="Greedy Open Source",
+      maintainer="Sergey Klimov",
+      maintainer_email="dcdarv@gmail.com",
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: End Users/Desktop',
+          'License :: OSI Approved :: Python Software Foundation License',
+          'Operating System :: MacOS :: MacOS X',
+          'Operating System :: Microsoft :: Windows',
+          'Operating System :: POSIX',
+          'Programming Language :: Python',
+          ],
+      cmdclass = cmds,
+      
+     )
+
+
+
+if sys.platform == 'darwin':
+    extra_options = dict(
+        setup_requires=['py2app'],
+        app=[mainscript],
+        # Cross-platform applications generally expect sys.argv to
+        # be used for opening files.
+        options=dict(py2app=dict(argv_emulation=True)),
+
+    )
+elif sys.platform == 'win32':
+
+    import sys
+
+    sys.path.append(r'c:/Program Files/Microsoft Visual Studio 9.0/VC/redist/x86/Microsoft.VC90.CRT')
+    sys.path.append(r'c:\Python27\vcruntime')
+    sys.path.append(r'c:\Python26\vcruntime')
+    
+    import py2exe
+    # Override the function in py2exe to determine if a dll should be included
+    dllList = ('mfc90.dll','msvcp90.dll','qtnetwork.pyd','qtxmlpatterns4.dll','qtsvg4.dll')
+    origIsSystemDLL = py2exe.build_exe.isSystemDLL
+    def isSystemDLL(pathname):
+        if os.path.basename(pathname).lower() in dllList:
+            return 0
+        return origIsSystemDLL(pathname)
+    py2exe.build_exe.isSystemDLL = isSystemDLL
+    extra_options = dict(
+        setup_requires=['py2exe'],
+        windows=[{
+            "script":mainscript,
+            "icon_resources": [(0, "resources/images/ico/cryotec_logo.ico")],
+            }],
+        options = {
+            py2exe : {
+                "includes" : ["sip", "PyQt4._qt", "cryotec_server", "qtdjango"]
+                }
+            },
+        
+        data_files = [('resources',glob.glob('data/*.png')),
+                      ('resources',glob.glob('data/*.ico')),
+                      ("Microsoft.VC90.CRT", glob.glob(r'c:\Python27\vcruntime\*.*'))],
+
+    )
+else:
+     extra_options = dict(
+         # Normally unix-like platforms will use "setup.py install"
+         # and install the main script as such
+         scripts=[mainscript],
+      )
+
+
+base_options.update(extra_options)
+options = base_options 
+
+
+
+setup( **options)
--- src/qbrainspell.py
+++ src/qbrainspell.py
@@ -6,6 +6,8 @@ import PyQt4
 from PyQt4.QtGui import *
 from PyQt4.QtCore import *
 import brainspell
+import images_rc
+from utils import odict
 
 
 class NewGameDialog(QDialog):
@@ -46,31 +48,23 @@ class DemonName(QLabel):
         self.set_name(self.game.demon_name)
 
 class PlaygroundPiece(QGraphicsItem):
-    __symbols = {
-        "+":"+",
-        "-":"-",
-        "<":"<",
-        ">":">",
-        ".":".",
-        ",":",",
-        "[":"[",
-        "]":"]",
-        "/":"↻",
-        "\\":"↺",
-    }
-    def __init__(self, game, coord, parent=None, scene=None):
+    def __init__(self, game, coord, operator_actions, parent=None, scene=None):
         super(PlaygroundPiece, self).__init__(parent=parent, scene=scene)
         self.game, self.coord = game, coord
         self.active = False
+        self.operator_actions = operator_actions
         self.setAcceptedMouseButtons(Qt.LeftButton)
         
     def paint(self, painter, option, widget):
         br = self.boundingRect()
         try:
-            painter.drawText(br.bottomLeft(), \
-                self.__symbols[self.game.gamemap.get_bfoperator(self.coord).operator])
+            op = self.game.gamemap.get_bfoperator(self.coord).operator
         except AttributeError:
-            pass
+            op = None
+        if op is not None:            
+            painter.drawImage(br, 
+                self.operator_actions[op].qimage)
+            print op, "draw"
         #painter.drawText(QPointF(x,y2), "8")
         if self.coord==self.game.current_coord:
             painter.fillRect(self.boundingRect(), QColor(32,11,33))
@@ -96,13 +90,13 @@ class PlaygroundPiece(QGraphicsItem):
         for piece in self.scene().pieces:
             if piece.coord == old_active_coord:
                 piece.update()
-        print "item"
         
 class PlaygroundScene(QGraphicsScene):
     piece_size = 30.0
-    def __init__(self, parent=None, game=None):
+    def __init__(self, operator_actions, parent=None, game=None):
         super(PlaygroundScene, self).__init__(parent=parent)
         self.game = game
+        self.operator_actions = operator_actions
         self.pieces = []
         
         for i in range(self.game.gamemap.size_x+1):
@@ -112,14 +106,15 @@ class PlaygroundScene(QGraphicsScene):
         
         for x in range(self.game.gamemap.size_x):
             for y in range(self.game.gamemap.size_y):
-                piece = PlaygroundPiece(self.game, brainspell.Coords(x,y))
+                piece = PlaygroundPiece(self.game, brainspell.Coords(x,y), self.operator_actions)
                 self.pieces.append(piece)
                 self.addItem(piece)
                 
         
 class Playground(QGraphicsView):
-    def __init__(self, parent=None, game=None):
+    def __init__(self, operator_actions, parent=None, game=None):
         super(Playground, self).__init__(parent=parent)
+        self.operator_actions = operator_actions
         self.game = game
         self.redraw_game()
         
@@ -128,68 +123,10 @@ class Playground(QGraphicsView):
     
     def redraw_game(self):
         if self.game is not None:
-            self.scene = PlaygroundScene(parent=self, game=self.game)
+            self.scene = PlaygroundScene(parent=self, game=self.game, operator_actions=self.operator_actions)
             self.setScene(self.scene) 
             self.show()
-            
-           
-class ManaBar(QProgressBar):
-    def text(self):
-        return "%d" % self.value()
-
-class PlayerWidget(QWidget):
-    def __init__(self, player, parent=None):
-        super(PlayerWidget, self).__init__(parent)
-        self.player = player
-        layout = QVBoxLayout()
-        self.setLayout(layout)
-        self.name_label = QLabel()
-        self.mana_bar = ManaBar()
-        layout.addWidget(self.name_label)
-        layout.addWidget(self.mana_bar)
-        
-    def redraw_game(self):
-        self.mana_bar.setMaximum(self.player.max_mana)
-        self.name_label.setText(self.player.name)
-    
-    def tick(self):
-        self.mana_bar.setValue(self.player.mana)
-
-
-
-
-class CastsWidget(QWidget):
-    def __init__(self, player, sidebar, parent=None):
-        super(CastsWidget, self).__init__(parent)
-        self.player = player
-        self.sidebar = sidebar
-        layout = QVBoxLayout()
-        self.setLayout(layout)
-        self.casts = {}
-        
-    def redraw_game(self):
-        avail_casts = self.player.get_casts()
-        for castname, castcost in avail_casts.items():
-            button = QPushButton(castname.replace("_"," ").capitalize())
-            self.casts[castname] = (button, castcost)
-            self.layout().addWidget(button)
-            #FIXME!
-            #button.clicked.connect(lambda: self.player.cast(castname))
-        self.casts["run"][0].clicked.connect(lambda: self.player.cast("run"))
-        self.casts["stop"][0].clicked.connect(lambda: self.player.cast("stop"))
-        self.casts["create_robot"][0].clicked.connect(self.create_robot)
-            
-    def create_robot(self):
-        self.player.cast("create_robot", self.player.game.current_coord, self.player.game.current_dir)
-        self.sidebar.redraw_game()
-    
-    def tick(self):
-        for castname, (button, castcost) in self.casts.items():
-            button.setEnabled((castcost<=self.player.mana))
-
-
-
-
+ 
 class RobotWidget(QWidget):
     def __init__(self, robot, parent=None):
         super(RobotWidget, self).__init__(parent)
@@ -202,7 +139,6 @@ class RobotWidget(QWidget):
         layout.addWidget(self.debug)
         
     def redraw_game(self):
-        print "hi robo"
         pass
     
     def tick(self):
@@ -221,22 +157,96 @@ class RobotWidget(QWidget):
                            self.robot.direction.__unicode__())
 
 
+           
+           
+class ManaBar(QProgressBar):
+    def text(self):
+        return "%d" % self.value()
+
+class PlayerWidget(QWidget):
+    def __init__(self, player, parent=None):
+        super(PlayerWidget, self).__init__(parent)
+        self.player = player
+        self.vblayout = layout = QVBoxLayout()
+        self.setLayout(layout)
+        self.name_label = QLabel()
+        self.mana_bar = ManaBar()
+        layout.addWidget(self.name_label)
+        layout.addWidget(self.mana_bar)
+        self.robots = []
+        
+    def redraw_game(self):
+        self.mana_bar.setMaximum(self.player.max_mana)
+        self.name_label.setText(self.player.name)
+        for robot in self.player.robots:
+            self.add_robot(robot)
+                        
+    def add_robot(self, robot):
+        r = RobotWidget(robot)
+        self.vblayout.addWidget(r)
+        self.robots.append(r)
+        r.redraw_game()
+        
+
+    
+    def tick(self):
+        for robot in self.player.robots:
+            new = True
+            for robot_w in self.robots:
+                if robot_w.robot == robot:
+                    new = False
+            if new:
+                print "new robot"
+                self.add_robot(robot)
+                
+        for robot in self.robots:
+            robot.tick()
+        
+        self.mana_bar.setValue(self.player.mana)
+
+
+
+
+class CastsWidget(QWidget):
+    def __init__(self, player, cast_actions, parent=None):
+        super(CastsWidget, self).__init__(parent)
+        self.player = player
+        self.cast_actions = cast_actions
+        layout = QVBoxLayout()
+        self.setLayout(layout)
+        self.casts = {}
+        
+    def redraw_game(self):
+        avail_casts = self.player.get_casts()
+        for castname, castcost in avail_casts.items():
+            button = QPushButton(self.cast_actions[castname].text())
+            button.clicked.connect(self.cast_actions[castname].triggered)
+            self.casts[castname] = (button, castcost)
+            self.layout().addWidget(button)
+            
+    def tick(self):
+        for castname, (button, castcost) in self.casts.items():
+            button.setEnabled((castcost<=self.player.mana))
+            self.cast_actions[castname].setEnabled((castcost<=self.player.mana))
+
+
+
+
 
 
 class SideDock(QDockWidget):
-    def __init__(self, name, parent=None, game=None):
+    def __init__(self, name, cast_actions, operator_actions, parent=None, game=None):
         super(SideDock, self).__init__(name, parent)
         self.game = game
+        self.cast_actions, self.operator_actions = cast_actions, operator_actions
         w = QWidget()
         self.vblayout = QVBoxLayout()
         w.setLayout(self.vblayout)
         self.setWidget(w)
         self.players = []
-        self.robots = {}
     
     def redraw_game(self):
         self.players = []
-        self.robots = {}
        
         #fixme!
         while True:
@@ -254,25 +264,26 @@ class SideDock(QDockWidget):
         for player in self.game.players:
             player_w = PlayerWidget(player)
             self.players.append(player_w)
-            self.robots[player.name] = []
             self.vblayout.addWidget(player_w)
-            for robot in player.robots:
-                robot_widget = RobotWidget(robot)
-                self.robots[player.name].append(robot_widget)
-                self.vblayout.addWidget(robot_widget)
+
         self.vblayout.addItem(QSpacerItem(20, 20, \
                     QSizePolicy.Minimum, QSizePolicy.Expanding))
         
-        self.casts = CastsWidget(parent=self, player=self.game.current_player, sidebar=self)
+        self.casts = CastsWidget(parent=self, player=self.game.current_player, cast_actions=self.cast_actions)
         self.vblayout.addWidget(self.casts)
         
         
         for player in self.players:
             player.redraw_game()
-        for playername, robots in self.robots.items():
-            for robot in robots:
-                robot.redraw_game()
+
         self.casts.redraw_game()
+        
+        oper_toolbar = QToolBar()
+        self.vblayout.addWidget(oper_toolbar)
+        for operator, oa in self.operator_actions.items():
+            print operator
+            oper_toolbar.addAction(oa)
+            
        
                
             
@@ -280,9 +291,6 @@ class SideDock(QDockWidget):
     def tick(self):
         for player in self.players:
             player.tick()
-        for playername, robots in self.robots.items():
-            for robot in robots:
-                robot.tick()
                 
         self.casts.tick()
     
@@ -290,7 +298,27 @@ class SideDock(QDockWidget):
 
 
 class MainForm(QMainWindow):
+       
     def __init__(self):
+        self.__casts = odict({
+            "run": (u"Run robots", None),
+            "stop": (u"Stop robots", None),
+            "create_robot": (u"Create robot", self.create_robot),
+        })
+        
+        self.__operators = odict.from_tuple((
+           ( ">", (u"Next register", "greaterthan", None)),
+           ( "<", (u"Previous register", "lessthan", None)),
+           ( "+", (u"Increase", "plus", None)),
+           ( "-", (u"Decrease", "minus", None)),
+           ( "[", (u"Begin cycle", "leftbracket", None)),
+           ( "]", (u"End cycle", "rightbracket", None)),
+           ( ".", (u"Output current register", "dot", None)),
+           ( ",", (u"Input to current register", "comma", None)),
+           ( "/", (u"Rotate robot clockwise", "clockwise", None)),
+           ( "\\", (u"Rotate robot anticlockwise", "clockwise", None)),
+        ))
+
         super(MainForm, self).__init__()
         self.resize(800,600)
         self.setWindowTitle('QBrainSpell')
@@ -313,9 +341,36 @@ class MainForm(QMainWindow):
         step_action = QAction(u"Step", self)
         step_action.triggered.connect(self.step)
 
-        
         menubar = self.menuBar()
         file_menu = menubar.addMenu(u"&Game")
+        operators_menu = menubar.addMenu(u"&Operators")
+        casts_menu = menubar.addMenu(u"&Casts")
+        help_menu = menubar.addMenu(u"&Help")
+ 
+        self.cast_actions = odict()
+        for cast, (casttitle, func) in self.__casts.items():
+            ca = QAction(casttitle, self)
+            if func is None:
+                ca.triggered.connect(lambda: self.game.current_player.cast(cast))
+            else:
+                ca.triggered.connect(func)
+            self.cast_actions[cast] = ca
+            casts_menu.addAction(ca)
+            
+         
+        self.operator_actions = odict()
+        for operator, (optitle, opiconname, func) in self.__operators.items():
+            oa = QAction(QIcon(":/"+opiconname+".svg"),optitle, self)
+            oa.qimage = QImage(":/"+opiconname+".svg")
+            if func is None:
+                oa.triggered.connect(lambda: self.game.current_player.place_operator(operator, self.game.current_coord))
+            else:
+                oa.triggered.connect(func)
+            self.operator_actions[operator] = oa
+            operators_menu.addAction(oa)
+            
+
+        
         file_menu.addAction(new_game_action)
         file_menu.addAction(play_pause_action)
         file_menu.addAction(step_action)
@@ -328,8 +383,8 @@ class MainForm(QMainWindow):
         widg.setLayout(layout)
         self.setCentralWidget(widg)
         self.demon_name = DemonName()
-        self.playground = Playground()
-        self.sidebar = SideDock(u"Information", self)
+        self.playground = Playground(operator_actions = self.operator_actions)
+        self.sidebar = SideDock(u"Information", self.cast_actions, self.operator_actions, self)
         self.sidebar.setAllowedAreas(Qt.RightDockWidgetArea)
         self.addDockWidget(Qt.RightDockWidgetArea, self.sidebar)
         sidebar_action = self.sidebar.toggleViewAction()
@@ -342,16 +397,17 @@ class MainForm(QMainWindow):
         toolbar.addAction(sidebar_action)
         toolbar.addAction(play_pause_action)
         toolbar.addAction(step_action)
-        
+               
         self.demo()
     
     def new_game(self):
         d = NewGameDialog(self)
         d.exec_()
         if d.result()==QDialog.Accepted:
-            self.create_game(d.size_x.value(), d.size_y.value(), d.players.value(), "middle")
+            self.create_game(brainspell.Map(d.size_x.value(), d.size_y.value()), d.players.value(), "middle")
             
             self.redraw_game()
+            
     def demo(self):
         demo_map = [\
 r"+++/",\
@@ -360,21 +416,19 @@ r"   +",\
 r".++/",\
                    ]
         gamemap = brainspell.Map.from_list(demo_map)
-        self.game = brainspell.Game(gamemap, "great")
-        self.game.current_player = brainspell.Player(u"Player #%d"%1, self.game)
-        self.game.current_coord = brainspell.Coords(0,0)
-        self.game.current_dir = brainspell.Direction('e')
-        
+        self.create_game(gamemap, 1, "great")
+       
         self.redraw_game()
         self.play_pause(True)
             
-    def create_game(self, x, y, players_num, gametype):
-        gamemap = brainspell.Map(x,y)
+    def create_game(self, gamemap, players_num, gametype):
         self.game = brainspell.Game(gamemap, gametype)
         for i in range(players_num):
             pl = brainspell.Player(u"Player #%d"%i, self.game)
         self.game.current_player = self.game.players[0]
-        
+        self.game.current_coord = brainspell.Coords(0,0)
+        self.game.current_dir = brainspell.Direction('e')
+       
     
     def redraw_game(self):
         self.demon_name.game = self.game
@@ -403,8 +457,7 @@ r".++/",\
             map_list = [line.rstrip() for line in f.readlines()]
             f.close()
             gamemap = brainspell.Map.from_list(map_list)
-            self.game = brainspell.Game(gamemap, "middle")
-            pl = brainspell.Player(u"Player #1", self.game)
+            self.create_game(gamemap, 1, "middle")
             self.redraw_game()
             
         
@@ -428,8 +481,10 @@ r".++/",\
         else:
             self.play_pause_action.setText(u"Play")
             self.tick_timer.stop()
+    def create_robot(self):
+        self.game.current_player.cast("create_robot", self.game.current_coord, self.game.current_dir)
     
-        
+   
 if __name__=="__main__":
     app = QApplication(sys.argv)
     w = MainForm()
